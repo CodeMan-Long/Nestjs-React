e2c6ebd586ebb9d9f635b58911b8b593aa941138
code man <top.talented@outlook.com>
2022-10-04 13:02:25 +0800

    feat(frontend): add home page

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
     <div>
-      <Button variant="contained">Set Statistic</Button>
-      <Button variant="contained">View Statistics</Button>
+      <Button component={Link} to="/save" variant="contained" sx={{ m: 0.5 }}>
+        Set Statistic
+      </Button>
+
+      <Button component={Link} to="/view" variant="contained" sx={{ m: 0.5 }}>
+        View Statistic
+      </Button>
     </div>
   );
 }
